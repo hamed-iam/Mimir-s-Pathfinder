f2a53f83ca151fc52932d1032f22a37e66d4ca6a
Hamed Ghazali <hamedghazali53@gmail.com>
2024-03-13 18:39:37 +0330

    add draggin a point functionality

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -4,25 +4,55 @@ class GraphEditor {
     this.graph = graph;
     this.ctx = this.canvas.getContext("2d");
     this.selected = null;
+    this.dragging = false;
     this.hovered = null;
     this.#addEventListeners();
   }
 
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", (evt) => {
+      if (evt.button == 2) {
+        //right click
+        if (this.hovered) {
+          this.#removePoint(this.hovered);
+        }
+      }
+      if (evt.button == 0) {
+        // left click
+        const mouse = new Point(evt.offsetX, evt.offsetY);
+        if (this.hovered) {
+          this.selected = this.hovered;
+          this.dragging = true;
+          return;
+        }
+        this.graph.addPoint(mouse);
+        this.selected = mouse;
+        this.hovered = mouse;
+      }
+    });
+    this.canvas.addEventListener("mousemove", (evt) => {
       const mouse = new Point(evt.offsetX, evt.offsetY);
       this.hovered = getNearestPoint(mouse, this.graph.points, 10);
-      if (this.hovered) {
-        this.selected = this.hovered;
-        return;
+      if (this.dragging) {
+        this.selected.x = mouse.x;
+        this.selected.y = mouse.y;
       }
-      this.graph.addPoint(mouse);
-      this.selected = mouse;
     });
+    this.canvas.addEventListener("contextmenu", (e) => e.preventDefault());
+    this.canvas.addEventListener("mouseup", () => (this.dragging = false));
+  }
+
+  #removePoint(point) {
+    this.graph.removePoint(point);
+    this.hovered = null;
+    if (this.selected == point) this.selected = null;
   }
 
   display() {
     this.graph.draw(this.ctx);
+    if (this.hovered) {
+      this.hovered.draw(this.ctx, { fill: true });
+    }
     if (this.selected) {
       this.selected.draw(this.ctx, { outline: true });
     }
